6abf80baf5a0d7cc07ee63b3e87e25c0ad7f2c9a
colossus06 <denizkavuk86@gmail.com>
2024-01-15 01:48:43 +0300

    activate

---
+++ _static/activate.js
@@ -0,0 +1,6 @@
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://platform-api.sharethis.com/js/sharethis.js#property=65a46290fbfe1600199c5be9&product=sop';
+script.async = true;
+
+document.head.appendChild(script);
